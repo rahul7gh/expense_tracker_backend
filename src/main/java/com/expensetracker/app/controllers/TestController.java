f9c3d6c65d01757c5233efe5250559ac709bc85c
rahul7gh <30512797+rahul7gh@users.noreply.github.com>
2023-02-03 22:58:25 +0530

    Create TestController.java

---
+++ src/main/java/com/expensetracker/app/controllers/TestController.java
@@ -0,0 +1,14 @@
+package com.expensetracker.app.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	@GetMapping
+	public String tesHello()
+	{
+		return "Aayeah!";
+	}
+}
